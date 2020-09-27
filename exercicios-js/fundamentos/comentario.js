832327ceca1f67d977917c6efacb2cf68487cced
Lucas Souza <lhenriquesouza00@gmail.com>
2020-09-27 13:55:06 -0300

    [JavaScript-Fundamentos] Comentários de Código

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentario de uma linha
+console.log('Linha 1');
+
+/*
+Comentário de
+Multiplas linhas
+*/
+console.log('Linha 2');
+
+/*
+ * Comentario de
+ * múltiplas linhass
+ */
+console.log('linha 3');
