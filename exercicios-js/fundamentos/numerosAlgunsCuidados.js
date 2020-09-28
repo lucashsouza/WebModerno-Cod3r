59025b220c90da9cd8b9d3532e3731e78f819abe
lucashsouza <lhenriquesouza00@gmail.com>
2020-09-28 16:37:43 -0300

    [JavaScript-Fundamentos] Number: Alguns Cuidados

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0.00001);
 console.log("10" / 2);
+console.log("3" + 2);    // Concatena, e vira string
+console.log("3" - 2);   // Realiza operação normalmente
 console.log("Show!" * 2);
 console.log(0.1 + 0.7);
 // console.log(10.toString());
