013eb88f98d4c0e31a76433bfe3d4ce9e9c0fe66
lucashsouza <lhenriquesouza00@gmail.com>
2020-10-02 15:13:28 -0300

    [JavaScript-Fundamentos] Tipos em JavaScript: Object

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const prod1 = {}
+prod1.nome = "Celular xxx 2020";
+prod1.preco = 2999.95;
+prod1['Desconto'] = 0.40;
+
+const prod2 = {
+    nome: "Camiseta Estampada",
+    preco: 79.85
+}
+
+
+console.log(prod1, prod2);
