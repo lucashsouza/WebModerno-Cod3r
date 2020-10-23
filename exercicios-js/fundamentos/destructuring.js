248703160f9093896899cf72039e23bb5517aa54
Lucas Souza <lhenriquesouza00@gmail.com>
2020-10-22 22:30:56 -0300

    [JavaScript-Fundamentos] Operadores: Destructuring #01

---
+++ exercicios-js/fundamentos/destructuring.js
@@ -0,0 +1,22 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+    nome: "Lucas",
+    idade: 20,
+    endereco: {
+        logradouro: "Rua Abc",
+        numero: 1000
+    }
+}
+
+const { nome, idade } = pessoa;
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa;
+console.log(n, i);
+
+const { sobrenome, bemHumorada = true } = pessoa;
+console.log(sobrenome, bemHumorada);
+
+const { endereco: { logradouro, numero, cep } } = pessoa;
+console.log(logradouro, numero, cep);
