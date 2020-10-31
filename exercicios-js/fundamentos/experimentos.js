639ba35e4153296e618a30c3796b12d570fefcfb
Lucas Souza <lhenriquesouza00@gmail.com>
2020-10-31 09:44:51 -0300

    [JavaScript-Fundamentos] Contexto de Execução: Browser vs Node

---
+++ exercicios-js/fundamentos/experimentos.js
@@ -0,0 +1,20 @@
+let a = 3;
+
+global.b = 123;
+
+this.c = 456;
+this.d = false;
+this.e = "teste";
+
+console.log(a);
+console.log(global.b);
+console.log(this.c);
+console.log(module.exports.c);
+console.log(module.exports === this);
+console.log(module.exports);
+
+// Criando variavel sem Let e var!
+abc = 123 // contexto global (não faça isso);
+console.log(global.abc);
+
+// module.exports = {e: 456, f: false, g:'teste'}
