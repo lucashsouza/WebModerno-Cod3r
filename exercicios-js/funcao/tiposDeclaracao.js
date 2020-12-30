163bd1a69f23cfaf141178327ba05deb1310224d
Lucas Souza <lhenriquesouza00@gmail.com>
2020-12-29 21:30:17 -0300

    [Javascript: Funcao] Tipos de Declaração

---
+++ exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,18 @@
+console.log(soma(3, 4))
+
+// function declaration
+function soma(x, y) {
+    return x + y;
+}
+
+// function expression
+const subtracao = function(x, y) {
+    return x - y;
+}
+console.log(subtracao(3, 4))
+
+// named function expression
+const multiplicacao = function multiplicacao(x, y) {
+    return x * y;
+}
+console.log(multiplicacao(3, 4))
